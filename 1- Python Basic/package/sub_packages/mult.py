527410ad5e84af2922300e4869a9af62f281aab8
nikesm98 <nikesm9818@gmail.com>
2024-08-30 00:03:12 +0530

    new files

---
+++ 1- Python Basic/package/sub_packages/mult.py
@@ -0,0 +1,2 @@
+def multipy(x,y):
+    return x*y
--- 1- Python Basic/test.py
+++ 1- Python Basic/test.py
@@ -1,4 +1,6 @@
 from package.maths import *
+from package.sub_packages.mult import *
 
 print(addition(2,3))
 print(subtraction(4,3))
+print(multipy(4,5))
