1f70a6e3e6a4507b40262d8c54b57b448c3c68f7
nikesm98 <nikesm9818@gmail.com>
2024-08-29 19:18:32 +0530

    files added

---
+++ 1- Python Basic/package/__init__.py
@@ -0,0 +1,3 @@
+''' __init__.py is a spwcial file used in python to define package
+ and initialize their namespace
+'''
---
+++ 1- Python Basic/test.py
@@ -0,0 +1,4 @@
+from package.maths import *
+
+print(addition(2,3))
+print(subtraction(4,3))
